15c03ac50bc11cc28b478b9a62cbe472a4a397ad
ivd <ivan@skalelabs.com>
2020-04-22 15:48:05 +0300

    Add latest tag to skaled images

--- scripts/skale_build/deploy_image.sh
+++ scripts/skale_build/deploy_image.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+set -e
 
 VERSION=$1
-DOCKER_USERNAME=$2
-DOCKER_PASSWORD=$3
-IMAGE_NAME=skalenetwork/schain:$VERSION
+BRANCH=$2
+DOCKER_USERNAME=$3
+DOCKER_PASSWORD=$4
+IMAGE_NAME="skalenetwork/schain:$VERSION"
+LATEST_IMAGE_NAME="skalenetwork/schain:$BRANCH-latest"
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
 docker push $IMAGE_NAME
+docker push $LATEST_IMAGE_NAME
